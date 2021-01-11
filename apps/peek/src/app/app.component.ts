08e4494aa6e4d90fba67655951e725767574ab8a
drposeidon0126 <drposeidon92@gmail.com>
2021-01-11 13:00:23 -0300

    initial commit

---
+++ apps/peek/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'peek-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'peek';
+}
