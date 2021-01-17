9c942aa73bc4540508238b4459ef19f9c0828fbf
drposeidon0126 <drposeidon92@gmail.com>
2021-01-17 01:35:53 -0300

    feat: upgrade audio

--- apps/crane/src/crane.gateway.ts
+++ apps/crane/src/crane.gateway.ts
@@ -3,19 +3,83 @@ import {
   ConnectedSocket,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets'
-import { Socket } from 'socket.io'
-import { Message } from '@peek/core/model'
+import { PeerAction } from '@peek/core/model'
+import { Server, Socket } from 'socket.io'
 
 @WebSocketGateway()
-export class CraneGateway {
-  @SubscribeMessage('message')
-  handleMessage(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() message: Message
-  ) {
-    console.log(message);
+export class CraneGateway implements OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server
 
-    socket.broadcast.send(message)
+  @SubscribeMessage(PeerAction.CreateOrJoin)
+  createOrJoin(
+    @ConnectedSocket() contact: Socket,
+    @MessageBody() peerContact: string
+  ) {
+    console.log('create or join to room ', peerContact)
+    const { rooms } = this.server.sockets.adapter
+    const peerRoom = rooms[peerContact] ?? { length: 0 }
+    const numPeers = peerRoom.length
+    console.log(`${peerContact} has ${numPeers} clients`)
+
+    if (numPeers == 0) {
+      contact.join(peerContact)
+      contact.emit(PeerAction.Created, peerContact)
+    } else if (numPeers == 1) {
+      contact.join(peerContact)
+      console.log('join: ', peerContact)
+      contact.emit(PeerAction.Joined, peerContact)
+    } else {
+      contact.emit(PeerAction.Full, peerContact)
+    }
+  }
+
+  @SubscribeMessage(PeerAction.Ready)
+  ready(
+    @ConnectedSocket() contact: Socket,
+    @MessageBody() peerContact: string
+  ) {
+    contact.broadcast.to(peerContact).emit(PeerAction.Ready)
+  }
+
+  @SubscribeMessage(PeerAction.Candidate)
+  candidate(
+    @ConnectedSocket() contact: Socket,
+    @MessageBody() peerContact: { room: string; cancidate: any }
+  ) {
+    contact.broadcast
+      .to(peerContact.room)
+      .emit(PeerAction.Candidate, peerContact)
+  }
+
+  @SubscribeMessage(PeerAction.Offer)
+  offer(
+    @ConnectedSocket() contact: Socket,
+    @MessageBody() peerContact: { room: string; sdp: any }
+  ) {
+    contact.broadcast
+      .to(peerContact.room)
+      .emit(PeerAction.Offer, peerContact.sdp)
+  }
+
+  @SubscribeMessage(PeerAction.Answer)
+  answer(
+    @ConnectedSocket() contact: Socket,
+    @MessageBody() peerContact: { room: string; sdp: any }
+  ) {
+    contact.broadcast
+      .to(peerContact.room)
+      .emit(PeerAction.Answer, peerContact.sdp)
+  }
+
+  handleDisconnect(@ConnectedSocket() contact: Socket) {
+    console.log('DISCONNCET', contact.id)
+    const { rooms } = this.server.sockets.adapter
+    if (rooms[contact.id]) {
+      contact.leaveAll()
+    }
   }
 }
--- apps/crane/src/main.ts
+++ apps/crane/src/main.ts
@@ -1,12 +1,9 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { CraneModule } from './crane.module'
-
 ;(async () => {
   const port = process.env.PORT || 3000
   const host = `http://localhost:${port}`
-  const crane = await NestFactory.create(CraneModule)
-  await crane.listen(port, () =>
-    Logger.log(`Listening at ${host}`, 'CraneGateway')
-  )
+  const crane = await NestFactory.create(CraneModule, { cors: true })
+  await crane.listen(port, () => Logger.log(`Listening at: ${host}`))
 })()
